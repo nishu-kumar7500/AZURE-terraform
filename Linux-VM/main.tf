0bf0cdbe044f5349ae5fd5d2289c670d66585b63
milton7500 <132784995+milton7500@users.noreply.github.com>
2024-06-16 14:52:10 +0530

    Update main.tf

--- Linux-VM/main.tf
+++ Linux-VM/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.108.0"
-    }
-  backend "azurerm" {
-    resource_group_name  = "bahubaliking"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "test989180"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
-
- }
-}
-
 provider "azurerm" {
     subscription_id = "809eff35-137f-4411-9bef-83c5a1552808"
     tenant_id = "f4425336-4592-4d59-8bf1-b90a45179899"
